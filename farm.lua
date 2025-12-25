1f30408cb143db5512165a24178fe21b5bc4dfc7
romankozeiko-cmd <romankozeiko@gmail.com>
2025-12-25 23:51:19 +0300

    Create farm.lua

---
+++ farm.lua
@@ -0,0 +1,15 @@
+while true do
+wait(20)
+game:GetService("Workspace").riperinside.HumanoidRootPart.CFrame = game:GetService("Workspace").Bosses.Waiting.Titan.qw.CFrame
+wait(11)
+if game:GetService("Workspace").RespawnMobs.Titan.Titan.Titan  then
+	game:GetService("Workspace").riperinside.HumanoidRootPart.CFrame = game:GetService("Workspace").RespawnMobs.Titan.Titan.CFrame
+end
+wait (20)
+game:GetService("Workspace").riperinside.HumanoidRootPart.CFrame = game:GetService("Workspace").Bosses.Waiting.Muscle.qw.CFrame
+wait(11)
+if game:GetService("Workspace").RespawnMobs.Muscle.Muscle then
+	game:GetService("Workspace").riperinside.HumanoidRootPart.CFrame = game:GetService("Workspace").RespawnMobs.Muscle.Muscle.CFrame
+end
+task.wait(1)
+end
